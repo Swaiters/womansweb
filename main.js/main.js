b6b54f5c714c5f9355b7ba0236da4d45aa294a60
Swaiters <90403530+Swaiters@users.noreply.github.com>
2023-03-19 23:18:36 -0400

    Add files via upload

--- main.js/main.js
+++ main.js/main.js
@@ -8,3 +8,21 @@ hamburger.addEventListener('click', () =>{
     : 'menu';
     mobile_menu.classList.toggle('is-open');
 })
+/*-----------------------------------------------------------------------------*/
+function small(){
+    console.log("I am in small")
+    bd = document.querySelector("body")
+    bd.style.fontSize = ".75em"
+}
+
+function medium(){
+    console.log("I am in medium")
+    bd = document.querySelector("body")
+    bd.style.fontSize = "1em"
+}
+
+function large(){
+    console.log("I am in large")
+    bd = document.querySelector("body")
+    bd.style.fontSize = "2em"
+}
